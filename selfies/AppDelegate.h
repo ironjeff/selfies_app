c29de0c3bf8d07ef8858f9318f1d35a1386f15c3
Jeff Huang <ironjeff@gmail.com>
2013-12-11 11:35:33 -0800

    Initial Commit

---
+++ selfies/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  selfies
+//
+//  Created by Jeff Huang on 12/11/13.
+//  Copyright (c) 2013 Jeff Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ selfies/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  selfies
+//
+//  Created by Jeff Huang on 12/11/13.
+//  Copyright (c) 2013 Jeff Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
